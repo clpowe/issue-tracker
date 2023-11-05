7c5ccf6854c66e09e783e82478f6838eb7e5edd6
clpowe <clpowe@gmail.com>
2023-11-04 22:08:04 -0400

    Build Issues Page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import { Button, TextArea, TextField } from '@radix-ui/themes'
+import React from 'react'
+
+function NewIssuesPage() {
+	return (
+		<div className='max-w-xl space-y-3'>
+			<TextField.Root>
+				<TextField.Input placeholder='Title' />
+			</TextField.Root>
+			<TextArea placeholder='Description' />
+			<Button>Submit new issue</Button>
+		</div>
+	)
+}
+
+export default NewIssuesPage
